75350b7c43d821a08d130546c56a0ecc3ff96718
ofoeduchisom <ofoeduchisomd@gmail.com>
2022-05-07 16:23:39 +0000

    first commit

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const data = {
+    products: [
+      {
+        id: '1',
+        name: 'Macbook',
+        price: 1400,
+        image: 'https://images.macrumors.com/article-new/2020/11/macbook-air-m1-unboxing.jpg',
+
+     },
+     {
+        id: '2',
+        name: 'Smart Tv',
+        price: 400,
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdkxrF3yP5uHvAAPT-IE4q2FFTM4yRA9zRYw&usqp=CAU',
+        
+     },
+     {
+        id: '3',
+        name: 'Adidas Sneakers',
+        price: 700,
+        image: 'https://i.pinimg.com/564x/b0/3d/b6/b03db6571ca1cf29918b45dd6146d3dc.jpg',
+
+     },
+   
+     
+   ],
+};
+export default data;
