549e6c28f2401b05b6002bf49947a7497b71c70e
ofoeduchisom <ofoeduchisomd@gmail.com>
2022-05-07 09:53:44 +0000

    first commit

---
+++ shopping-cart/src/data.js
@@ -0,0 +1,49 @@
+const data = {
+    products: [
+      {
+        id: '1',
+        name: 'Macbook',
+        price: 1400,
+        image: 'https://images.macrumors.com/article-new/2020/11/macbook-air-m1-unboxing.jpg',
+
+     },
+     {
+        id: '2',
+        name: 'Joggers',
+        price: 400,
+        image: 'https://media.self.com/photos/61aaa937f1d6fe45bce6a354/1:1/w_1000,h_1000,c_limit/Under%20Armour%20UA%20Rival%20Fleece%20Joggers.jpg',
+
+     },
+     {
+        id: '3',
+        name: 'Adidas Sneakers',
+        price: 700,
+        image: 'https://i.pinimg.com/564x/b0/3d/b6/b03db6571ca1cf29918b45dd6146d3dc.jpg',
+
+     },
+     {
+        id: '4',
+        name: 'TV',
+        Price: 800,
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8LZOkFsicGd3-W69dkAPHbhy2e2WwNncJ1w&usqp=CAU',
+     },
+     {
+      id: '3',
+      name: 'Adidas Sneakers',
+      price: 700,
+      image: 'https://i.pinimg.com/564x/b0/3d/b6/b03db6571ca1cf29918b45dd6146d3dc.jpg',
+
+   },
+   
+   {
+   
+      id: '3',
+      name: 'Adidas Sneakers',
+      price: 700,
+      image: 'https://i.pinimg.com/564x/b0/3d/b6/b03db6571ca1cf29918b45dd6146d3dc.jpg',
+
+   },
+     
+   ],
+};
+export default data;
